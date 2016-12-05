9abb72dfcda13911b7fbf8833f22b0003ce08eb2
Jannis R <mail@jannisr.de>
2016-12-05 13:56:50 +0100

    init

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const express = require('express')
+const hsts = require('hsts')
+const corser = require('corser')
+const noCache = require('nocache')()
+const morgan = require('morgan')
+const sha3 = require('web3/lib/utils/sha3')
+const bodyParser = require('body-parser')
+const config = require('config')
+const spdy = require('spdy')
+const fs = require('fs')
+
+const verify = require('./verify')
+
+const api = express()
+module.exports = api
+
+api.use(hsts({maxAge: 3 * 24 * 60 * 60 * 1000})) // 3 days
+
+// CORS
+const allowed = corser.simpleRequestHeaders.concat(['User-Agent'])
+api.use(corser.create({requestHeaders: allowed}))
+
+api.use(bodyParser.json())
+
+morgan.token('email', (req) => sha3(req.query.email))
+morgan.token('address', (req) => req.query.address)
+api.use(morgan(':date[iso] :email :address :status :response-time ms'))
+
+api.post('/', noCache, verify)
+
+api.use((err, req, res, next) => {
+  if (res.headersSent) return next()
+  return res
+  .status(err.isBoom ? err.output.statusCode : 500)
+  .json({status: 'error', message: err.message})
+})
+
+const server = spdy.createServer({
+  cert: fs.readFileSync(config.http.cert),
+  key: fs.readFileSync(config.http.key)
+}, api)
+server.listen(config.http.port, (err) => {
+  if (err) return console.error(err)
+  console.info(`Listening on ${config.http.port}.`)
+})
---
+++ readme.md
@@ -0,0 +1,16 @@
+# [Parity](https://ethcore.io/parity.html) e-mail verification
+
+[![Join the chat at https://gitter.im/ethcore/parity][gitter-image]][gitter-url] [![GPLv3][license-image]][license-url]
+
+[gitter-image]: https://badges.gitter.im/Join%20Chat.svg
+[gitter-url]: https://gitter.im/ethcore/parity
+[license-image]: https://img.shields.io/badge/license-GPL%20v3-green.svg
+[license-url]: https://www.gnu.org/licenses/gpl-3.0.en.html
+
+## Installation
+
+```shell
+git clone https://github.com/ethcore/email-verification.git
+cd email-verification
+npm install --production
+```
